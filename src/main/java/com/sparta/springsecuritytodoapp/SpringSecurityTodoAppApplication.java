635a4ea1731641652716c2f6a393358f702b7a50
EugeneChoi <100894921+geneeuchoi@users.noreply.github.com>
2024-05-31 09:48:07 +0900

    init commit

---
+++ src/main/java/com/sparta/springsecuritytodoapp/SpringSecurityTodoAppApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.springsecuritytodoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityTodoAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityTodoAppApplication.class, args);
+    }
+
+}
